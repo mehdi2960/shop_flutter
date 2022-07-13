dca9d673efff057362c08eaa692e36571a774139
mehdi <mehdiprogrammer30@gmail.com>
2022-07-13 22:45:14 +0430

    back button

--- lib/screen/details/detail.dart
+++ lib/screen/details/detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '../../constants.dart';
 import '../../moduls/product.dart';
 
 class DetailScreen extends StatelessWidget {
@@ -7,6 +9,40 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: product.color,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: product.color,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: SvgPicture.asset(
+            "assets/icons/back.svg",
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/search.svg",
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/cart.svg",
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+        ],
+      ),
+    );
   }
 }
